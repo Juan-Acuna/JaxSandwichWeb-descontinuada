864ec2059f7b84c3802d86dc9acab13a0ee054fe
= <=>
2021-08-06 15:29:20 -0400

    varios

---
+++ public/sandwichcord/latest/index.php
@@ -0,0 +1,20 @@
+<?php
+    $LATEST_VER = 'v0.7.0';
+    $LATEST_VER_LEGACY = 'v0.7.0';
+    if(isset($_GET['q'])){
+        $url = $_GET['q'];
+        if(isset($_GET['legacy'])){
+            if(str_starts_with($url, '/javadoc/com/jaxsandwich/sandwichcord/')){
+                header('Location: http://jaxsandwich.com/sandwichcord/'.$LATEST_VER_LEGACY.$url);
+                exit();
+            }else{
+                exit();
+            }
+        }else{
+            #no implementado aun.
+        }
+    }else{
+        header('Location: http://jaxsandwich.com/sandwichcord/'.$LATEST_VER_LEGACY.'/');
+        exit();
+    }
+?>
