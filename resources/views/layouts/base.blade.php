bbf8e301972bfcb6b649c6deea4ecb013d116e57
= <=>
2021-08-09 19:06:37 -0400

    varios

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -6,8 +6,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
         <title>@yield('title')</title>
-        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-        <link rel="stylesheet" href="css/estilo.css">
+        <link rel="shortcut icon" href="../../../public/favicon.png" type="image/x-icon">
+        <link rel="stylesheet" href="../../css/estilo.css">
     </head>
     <body class="bg-dark text-white">
         <nav class="navbar navbar-dark navbar-expand-lg bg-danger text-white">
