1f5e1f2ffb66a8e670b587127d3866a2f4312f8e
= <=>
2021-08-09 13:29:30 -0400

    varios

--- public/index.php
+++ public/index.php
@@ -1,3 +1,37 @@
-<?php
-    require_once "index.html";
-?>
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+        <title>Jax Sandwich</title>
+        <link rel="shortcut icon" href="src/ico/favicon.png" type="image/x-icon">
+        <link rel="stylesheet" href="http://src.jaxsandwich.com/css/estilo.css" crossorigin="anonymous">
+        <link rel="stylesheet" href="../public/src/css/estilo.css">
+    </head>
+    <body class="bg-dark text-white">
+        <php?
+            require_once "src/include/navbar.html";
+        ?>
+        <div class="container-fluid">
+            <div class="row">
+                <h1 class="col-12 text-center p-3">Página en construcción...</h1>
+                <hr>
+            </div>
+            <div class="row mt-3">
+                <div class="col-xs-12 col-md-6 p-3">
+                    <p class="tarjeta p-5 bg-white text-dark">Si vienes por la documentación de Sandwichcord Framework, 
+                        <a href="http://sandwichcord.jaxsandwich.com/">Haz click en este enlace.</a>
+                    </p>
+                </div>
+                <div class="col-xs-12 col-md-6 p-3">
+                    <p class="tarjeta p-5 bg-white text-dark">Si vienes por la documentación de Jax Sandwich Bot, 
+                        <a href="http://bot.jaxsandwich.com/">Haz click en este enlace.</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    </body>
+</html>
