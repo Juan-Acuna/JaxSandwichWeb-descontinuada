a22a6d2aba8cf85f9497c9cfbbfc4bcf9cc1b8fe
= <=>
2021-08-09 13:54:02 -0400

    varios

--- public/src/include/navbar.php
+++ public/src/include/navbar.php
@@ -7,7 +7,7 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link text-white fs-5 fw-light active" aria-current="page" href="#">Inicio</a>
+                    <a class="nav-link text-white fs-5 fw-light active" aria-current="page" href="http://jaxsandwich.com/">Inicio</a>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle text-white fs-5 fw-light" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -15,16 +15,16 @@
                     </a>
                     <ul class="dropdown-menu bg-dark" aria-labelledby="navbarDropdownMenuLink">
                         <li>
-                            <a class="dropdown-item text-white fw-lighter" href="#">Sandwich Bots</a>
+                            <a class="dropdown-item text-white fw-lighter" href="http://sandwichbots.jaxsandwich.com/">Sandwich Bots</a>
                             <ul>
-                                <li class="dropdown-item text-secondary fs-6 fw-lighter">Sandwichcord Framework</li>
+                                <li><a class="dropdown-item text-secondary fs-6 fw-lighter" href="http://sandwichcord.jaxsandwich.com/">Sandwichcord Framework</a></li>
                             </ul>
                         </li>
-                        <li><a class="dropdown-item text-white fw-lighter" href="#">Jax Sandwich Bot</a></li>
+                        <li><a class="dropdown-item text-white fw-lighter" href="http://bot.jaxsandwich.com/">Jax Sandwich Bot</a></li>
                     </ul>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-white fs-5 fw-light" href="#">Personas</a>
+                    <a class="nav-link text-white fs-5 fw-light" href="http://personas.jaxsandwich.com/">Personas</a>
                 </li>
             </ul>
         </div>
