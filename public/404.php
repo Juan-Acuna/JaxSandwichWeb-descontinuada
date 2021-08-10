4035d1647a862ff35703947020ef65bc0eee2598
= <=>
2021-08-10 18:00:27 -0400

    javadoc intento 26

--- public/404.php
+++ public/404.php
@@ -1,12 +1,6 @@
 <?php
-    echo '**************************************';
-    echo 'URL: ';
-    var_dump($_REQUEST[0]);
-    var_dump($_REQUEST[26]);
-    var_dump($_REQUEST["REQUEST_URI"]);
-    echo '**************************************<br><br>';
-    if(str_ends_with($_REQUEST["REQUEST_URI"],'$')){
-        header('Location : //jaxsandwich.com'.substr($_REQUEST["REQUEST_URI"],0,-1));
+    if(str_ends_with($_SERVER["REQUEST_URI"],'$')){
+        header('Location : //jaxsandwich.com'.substr($_SERVER["REQUEST_URI"],0,-1));
         exit();
     }else{
         var_dump($_REQUEST);
