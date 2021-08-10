68226f7ea1c58cff0997a8265b8ea1d26b775b08
= <=>
2021-08-10 18:05:38 -0400

    test

--- public/404.php
+++ public/404.php
@@ -1,6 +1,6 @@
 <?php
     if(str_ends_with($_SERVER["REQUEST_URI"],'$')){
-        header('Location : //jaxsandwich.com'.substr($_SERVER["REQUEST_URI"],0,-1));
+        //header('Location : //jaxsandwich.com'.substr($_SERVER["REQUEST_URI"],0,-1));
         exit();
     }else{
         var_dump($_REQUEST);
