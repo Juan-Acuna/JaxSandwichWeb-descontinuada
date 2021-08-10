9f2f8d504a71974f134d220a1c1971a75f325288
= <=>
2021-08-10 17:31:05 -0400

    javadoc intento 22

--- public/404.php
+++ public/404.php
@@ -1,5 +1,9 @@
 <?php
-    var_dump($_REQUEST);
-    echo '<br><br>*************************************<br><br>';
-    var_dump($_SERVER);
+    if(str_ends_with($_REQUEST['REQUEST_URI'],'$')){
+        header('Location : //jaxsandwich.com'.substr($_REQUEST['REQUEST_URI'],0,-1));
+    }else{
+        var_dump($_REQUEST);
+        echo '<br><br>*************************************<br><br>';
+        var_dump($_SERVER);
+    }
 ?>
