e8812058936848231f9899681d7179098b283388
= <=>
2021-08-16 12:05:48 -0400

    css

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
         <title>@yield('title')</title>
-        <link rel="stylesheet" href="{{ asset('css/estilo.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         <link rel="shortcut icon" href="{{ asset('img/favicon.png') }}" type="image/x-icon">
     </head>
     <body class="bg-dark text-white">
