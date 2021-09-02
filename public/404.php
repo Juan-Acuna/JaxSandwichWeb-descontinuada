3bc7f50183cfe89d249a05db0ac924ba7ff034e8
= <=>
2021-09-02 17:08:17 -0400

    varios

--- public/404.php
+++ public/404.php
@@ -2,9 +2,9 @@
     if(str_ends_with($_SERVER["REQUEST_URI"],'$')){
         //header('Location : //jaxsandwich.com'.substr($_SERVER["REQUEST_URI"],0,-1));
         exit();
-    }else{
+    }/*else{
         var_dump($_REQUEST);
         echo '<br><br>*************************************<br><br>';
         var_dump($_SERVER);
-    }
+    }*/
 ?>
--- resources/views/dev/sandwichbots/sandwichcord/index.blade.php
+++ resources/views/dev/sandwichbots/sandwichcord/index.blade.php
@@ -5,14 +5,17 @@
         <h1 class="col-xs-12 text-center p-3">Sandwichcord Framework</h1>
     </div>
     <div class="row">
-        <h3 class="col-xs-12 col-md-6 p-3">Versiones diponibles</h3>
+        <a class="h3 col-xs-12 col-md-6 p-3" href="https://github.com/SandwichBotsTeam/sandwichcord-framework/releases">Versiones diponibles</a>
         <a class="h5 col-xs-12 col-md-6 p-3" href="https://github.com/SandwichBotsTeam/sandwichcord-framework">Repositorio GitHub</a>
     </div>
     <div class="row mt-3">
         <div class="col-xs-12 col-md-6 p-3">
             <div class="tarjeta p-5 bg-white text-dark">
-                <h5>Versión 0.6.x</h5>
-                <a href="//docs.jaxsandwich.com/sandwichcord/v0.6.x/javadoc">Documentación</a>
+                <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-success">Actual</span>
+                <h5>Versión 0.8.x</h5>
+                <a href="//docs.jaxsandwich.com/sandwichcord/v0.8.x/javadoc">Documentación</a>
+                <a href="https://github.com/SandwichBotsTeam/sandwichcord-framework/releases/tag/v0.8.0-beta">Descargar</a>
+                <!-- https://github.com/SandwichBotsTeam/sandwichcord-framework/releases -->
             </div>
         </div>
         <div class="col-xs-12 col-md-6 p-3">
@@ -21,11 +24,11 @@
                 <a href="//docs.jaxsandwich.com/sandwichcord/v0.7.x/javadoc">Documentación</a>
             </div>
         </div>
-        <div class="col-xs-12 col-md-6 p-3">
+        <!--<div class="col-xs-12 col-md-6 p-3">
             <div class="tarjeta p-5 bg-white text-dark">
-                <h5>Versión 0.8.x</h5>
-                <a href="//docs.jaxsandwich.com/sandwichcord/v0.8.x/javadoc">Documentación</a>
+                <h5>Versión 0.6.x</h5>
+                <a href="//docs.jaxsandwich.com/sandwichcord/v0.6.x/javadoc">Documentación</a>
             </div>
-        </div>
+        </div>-->
     </div>
 @endsection
