c489e35f6953622bff81a0783bf476058d9c465a
= <=>
2021-09-27 13:09:57 -0300

    version 0.8.1

--- resources/views/dev/sandwichbots/sandwichcord/index.blade.php
+++ resources/views/dev/sandwichbots/sandwichcord/index.blade.php
@@ -8,27 +8,50 @@
         <a class="h3 col-xs-12 col-md-6 p-3" href="https://github.com/SandwichBotsTeam/sandwichcord-framework/releases">Versiones diponibles</a>
         <a class="h5 col-xs-12 col-md-6 p-3" href="https://github.com/SandwichBotsTeam/sandwichcord-framework">Repositorio GitHub</a>
     </div>
-    <div class="row mt-3">
-        <div class="col-xs-12 col-md-6 p-3">
+        <div class="row mt-3"><div class="col-xs-12 col-md-6 p-3">
             <div class="tarjeta p-5 bg-white text-dark">
                 <span class="position-absolute note translate-middle badge rounded-pill bg-success">Actual</span>
-                <h5>Versión 0.8.x</h5>
+                <h5>Versión 0.8.1</h5>
                 <a href="//docs.jaxsandwich.com/sandwichcord/v0.8.x/javadoc">Documentación</a>
-                <a href="https://github.com/SandwichBotsTeam/sandwichcord-framework/releases/tag/v0.8.0-beta">Descargar</a>
-                <!-- https://github.com/SandwichBotsTeam/sandwichcord-framework/releases -->
+                <a href="https://github.com/SandwichBotsTeam/sandwichcord-framework/releases/tag/v0.8.1-beta">Descargar</a>
             </div>
         </div>
         <div class="col-xs-12 col-md-6 p-3">
             <div class="tarjeta p-5 bg-white text-dark">
-                <h5>Versión 0.7.x</h5>
+                <h5>Versión 0.8.0</h5>
+                <a href="//docs.jaxsandwich.com/sandwichcord/v0.8.x/javadoc">Documentación</a>
+            </div>
+        </div>
+        <div class="col-xs-12 col-md-6 p-3">
+            <div class="tarjeta p-5 bg-white text-dark">
+                <h5>Versión 0.7.2</h5>
+                <a href="//docs.jaxsandwich.com/sandwichcord/v0.7.x/javadoc">Documentación</a>
+            </div>
+        </div>
+        <div class="col-xs-12 col-md-6 p-3">
+            <div class="tarjeta p-5 bg-white text-dark">
+                <h5>Versión 0.7.1</h5>
+                <a href="//docs.jaxsandwich.com/sandwichcord/v0.7.x/javadoc">Documentación</a>
+            </div>
+        </div>
+        <div class="col-xs-12 col-md-6 p-3">
+            <div class="tarjeta p-5 bg-white text-dark">
+                <h5>Versión 0.7.0</h5>
                 <a href="//docs.jaxsandwich.com/sandwichcord/v0.7.x/javadoc">Documentación</a>
             </div>
         </div>
         <!--<div class="col-xs-12 col-md-6 p-3">
             <span class="position-absolute note translate-middle badge rounded-pill bg-danger">Obsoleto</span>
             <div class="tarjeta p-5 bg-white text-dark">
-                <h5>Versión 0.6.x</h5>
+                <h5>Versión 0.6.0</h5>
                 <a href="//docs.jaxsandwich.com/sandwichcord/v0.6.x/javadoc">Documentación</a>
+            </div>
+        </div>
+        <div class="col-xs-12 col-md-6 p-3">
+            <span class="position-absolute note translate-middle badge rounded-pill bg-danger">Obsoleto</span>
+            <div class="tarjeta p-5 bg-white text-dark">
+                <h5>Versión 0.5.0</h5>
+                <a href="//docs.jaxsandwich.com/sandwichcord/v0.5.x/javadoc">Documentación</a>
             </div>
         </div>-->
     </div>
