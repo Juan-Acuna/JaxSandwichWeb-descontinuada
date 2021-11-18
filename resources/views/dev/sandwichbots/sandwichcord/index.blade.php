879d5683b5be7cc2b9e38666acb6ecfb582ee2e2
= <=>
2021-11-18 12:06:46 -0300

    v0.9.0

--- resources/views/dev/sandwichbots/sandwichcord/index.blade.php
+++ resources/views/dev/sandwichbots/sandwichcord/index.blade.php
@@ -10,15 +10,14 @@
     </div>
         <div class="row mt-3"><div class="col-xs-12 col-md-6 p-3">
             <div class="tarjeta p-5 bg-white text-dark">
-                <span class="position-absolute note translate-middle badge rounded-pill bg-success">Snapshot</span>
-                <h5>Versión 0.9.0-r02</h5>
+                <span class="position-absolute note rounded-pill bg-success">Actual</span>
+                <h5>Versión 0.9.0</h5>
                 <a href="//docs.jaxsandwich.com/sandwichcord/v0.9.x/javadoc">Documentación</a>
                 <a href="https://github.com/SandwichBotsTeam/sandwichcord-framework/releases/tag/v0.9.0-beta">Descargar</a>
             </div>
         </div>
         <div class="row mt-3"><div class="col-xs-12 col-md-6 p-3">
             <div class="tarjeta p-5 bg-white text-dark">
-                <span class="position-absolute note translate-middle badge rounded-pill bg-success">Actual</span>
                 <h5>Versión 0.8.1</h5>
                 <a href="//docs.jaxsandwich.com/sandwichcord/v0.8.x/javadoc">Documentación</a>
                 <a href="https://github.com/SandwichBotsTeam/sandwichcord-framework/releases/tag/v0.8.1-beta">Descargar</a>
